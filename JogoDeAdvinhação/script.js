75e80b0b16aa7122e3f24ee3b1bb0ca33a82bce8
LucasLeite Duart <113530698+LucasLeiteDuarte@users.noreply.github.com>
2022-10-31 12:54:49 -0300

    Update script.js
    refatorando e algumas funcionalidades a mais

--- JogoDeAdvinhação/script.js
+++ JogoDeAdvinhação/script.js
@@ -1,39 +1,42 @@
+// variáveis
 const screen1 = document.querySelector(".screen1")
-const screen2 = document.querySelector(".screen2") 
-
-const randomNumber = Math.round(Math.random() * 10)
-let xAttempts = 1
-
-
-//function callBack
-function handleTryClick (event){
-    event.preventDefault() // não faça o padrão desse evento que é enviar o formulário.
-    
-    const inputNumber = document.querySelector("#inputNumber")
-
-
-    if(Number(inputNumber.value) == randomNumber) {
-        screen1.classList.add("hide")
-        screen2.classList.remove("hide")
-
-        document
-            .querySelector(".screen2 h2")
-            .innerText = `Acertou em ${xAttempts} tentativas`
-
-    
-    }
-        inputNumber.value = ""
-    xAttempts++
-
-}
-// Eventos
-
+const screen2 = document.querySelector(".screen2")
 const btnTry = document.querySelector("#btnTry")
 const btnReset = document.querySelector("#btnReset")
+let randomNumber = Math.round(Math.random() * 10)
+let xAttempts = 1
 
+// Eventos
 btnTry.addEventListener('click', handleTryClick)
-btnReset.addEventListener('click', function(){
-    screen1.classList.remove("hide")
-    screen2.classList.add("hide")
-    xAttempts = 1
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', function(e) {
+    if(e.key == 'Enter' && screen1.classList.contains('hide')) {
+        handleResetClick()
+    }
 })
+
+// funções
+function handleTryClick(event) {
+    event.preventDefault() // nao faca o padrao
+
+    const inputNumber = document.querySelector("#inputNumber")
+
+    if(Number(inputNumber.value) == randomNumber) {
+        toggleScreen()
+        screen2.querySelector("h2").innerText = `acertou em ${xAttempts} tentativas`
+    }
+
+    inputNumber.value = ""
+    xAttempts++
+}
+
+function handleResetClick() {
+    toggleScreen()
+    xAttempts = 1
+    randomNumber = Math.round(Math.random() * 10)
+}
+
+function toggleScreen() {
+    screen1.classList.toggle("hide")
+    screen2.classList.toggle("hide")
+}
